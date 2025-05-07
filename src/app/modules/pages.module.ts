f48bbaf95d49842538e43af893fbb0e174212244
Angel <u22217021@utp.edu.pe>
2025-05-07 11:13:31 -0500

    luis de mierda

--- src/app/modules/pages.module.ts
+++ src/app/modules/pages.module.ts
@@ -7,14 +7,18 @@ import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
+  // Lista de componentes, directivas y pipes declarados en este módulo
   declarations: [
-    PagesComponent
-
+    PagesComponent  // Declara que este módulo contiene el componente PagesComponent
   ],
+
+  // Otros módulos que se necesitan para que este módulo funcione
   imports: [
-    CommonModule,
-    PagesRoutingModule,
-    SharedModule
+    CommonModule,        // Importa directivas comunes como *ngIf, *ngFor (necesario en módulos que no son AppModule)
+    PagesRoutingModule,  // Importa las rutas específicas de este módulo (probablemente contiene rutas para PagesComponent)
+    SharedModule         // Módulo compartido con componentes/reutilizables que se usan en varios lugares
   ]
 })
+// Exporta la clase del módulo para que Angular pueda reconocerlo e integrarlo con otros módulos
 export class PagesModule { }
+
