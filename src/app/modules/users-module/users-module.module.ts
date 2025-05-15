f930b833ae779d43d7f60a7bb3e22faecacf21bd
Rafael Mejia <rafaelmejiafalcon@gmail.com>
2025-05-15 13:35:52 -0500

    componentes de entregas,ordenes,usuarios

--- src/app/modules/users-module/users-module.module.ts
+++ src/app/modules/users-module/users-module.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersModuleRoutingModule } from './users-module-routing.module';
+import { UsersComponent } from './users/users.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UsersComponent
+  ],
   imports: [
     CommonModule,
     UsersModuleRoutingModule
