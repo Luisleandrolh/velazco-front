64df719c4efff860a5776fe53bbfe61da62092b9
Luisleandrolh <leandrohuillca@gmail.com>
2025-05-15 13:17:40 -0500

    feat:servicio productos terminado

--- src/app/modules/inventory-module/services/inventario-service.service.ts
+++ src/app/modules/inventory-module/services/inventario-service.service.ts
@@ -28,6 +28,34 @@ export class InventarioServiceService {
     );
   }
 
+  
+  // PUT: Actualizar producto por ID
+actualizarProducto(id: number, producto: any): Observable<any> {
+  return this.http.put(`${this.apiUrl}/${id}`, producto).pipe(
+    catchError(this.handleError)
+  );
+}
+
+// DELETE: Eliminar producto por ID
+eliminarProducto(id: number): Observable<any> {
+  return this.http.delete(`${this.apiUrl}/${id}`).pipe(
+    catchError(this.handleError)
+  );
+}
+
+// PATCH: Cambiar estado "active" del producto por ID
+actualizarEstadoActivo(id: number, activo: boolean): Observable<any> {
+  return this.http.patch(`${this.apiUrl}/${id}/active`, { active: activo }).pipe(
+    catchError(this.handleError)
+  );
+}
+
+// GET: Obtener productos disponibles
+obtenerProductosDisponibles(): Observable<any> {
+  return this.http.get(`${this.apiUrl}/available`).pipe(
+    catchError(this.handleError)
+  );}
+
   private handleError(error: HttpErrorResponse) {
     console.error('Error en la petición:', error);
     return throwError(() => new Error('Hubo un problema con la solicitud. Inténtelo de nuevo más tarde.'));
