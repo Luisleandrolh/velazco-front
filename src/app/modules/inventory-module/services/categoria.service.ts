ed840a8f2409de48f0dccfe6835c05414a51bed1
Luisleandrolh <leandrohuillca@gmail.com>
2025-05-15 13:13:22 -0500

    feat:servicio categoria terminado

---
+++ src/app/modules/inventory-module/services/categoria.service.ts
@@ -0,0 +1,54 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  private dominio: string = environment.baseUrlApi;
+  private apiUrl: string = `${this.dominio}/api/categories`;
+
+
+  constructor(private http: HttpClient) {}
+
+  obtenerCategorias(): Observable<any> {
+    return this.http.get(this.apiUrl).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+   agregarCategoria(categoria: any): Observable<any> {
+    return this.http.post(this.apiUrl, categoria).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+
+  actualizarCategoria(id: number, categoria: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, categoria).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  
+  eliminarCategoria(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  
+
+
+
+  //un error generico para todos
+  private handleError(error: HttpErrorResponse) {
+      console.error('Error en la petición:', error);
+      return throwError(() => new Error('Hubo un problema con la solicitud. Inténtelo de nuevo más tarde.'));
+  }
+
+
+}
--- src/app/modules/inventory-module/services/inventario-service.service.ts
+++ src/app/modules/inventory-module/services/inventario-service.service.ts
@@ -1,7 +1,6 @@
-import { isPlatformBrowser } from '@angular/common';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
-import { catchError, Observable, of, throwError } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -12,27 +11,22 @@ export class InventarioServiceService {
   private dominio: string = environment.baseUrlApi;
   private apiUrl: string = `${this.dominio}/api/products`;
 
-  constructor(
-    private http: HttpClient,
-    @Inject(PLATFORM_ID) private platformId: Object // del lado del cliente
-  ) {}
+  constructor(private http: HttpClient) { 
+  }
 
 
   //get
-  obtenerProductos(): Observable<any> {
-    if (isPlatformBrowser(this.platformId)) {
-      return this.http.get(this.apiUrl);
-    } else {
-      // SSR: evitar hacer llamada HTTP y retornar datos vacíos o mock
-      return of([]);
-    }}
+  obtenerProductos(): Observable<any> {  //get
+    return this.http.get(`${this.apiUrl}`).pipe( 
+      catchError(this.handleError));
+  }
 
   //post
-  /*agregarProducto(producto: any): Observable<any> {
+  agregarProducto(producto: any): Observable<any> {
     return this.http.post(this.apiUrl, producto).pipe(
       catchError(this.handleError)
     );
-  }*/
+  }
 
   private handleError(error: HttpErrorResponse) {
     console.error('Error en la petición:', error);
